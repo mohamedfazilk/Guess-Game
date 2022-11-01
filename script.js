3db5031af4c2e3aa9611530ccb9c5c09ea88ac35
mohamedfazilk <2114921@cognizant.com>
2022-11-01 17:06:01 +0530

    place the again functionality

--- script.js
+++ script.js
@@ -7,9 +7,9 @@
 // document.querySelector('.guess').value=24
 // console.log(document.querySelector('.guess').value=24);
 
-const secretNumber = Math.trunc(Math.random() *20)+1;
+let secretNumber = Math.trunc(Math.random() *20)+1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber
+
 
 
 document.querySelector('.check').addEventListener(
@@ -18,12 +18,18 @@ document.querySelector('.check').addEventListener(
        const guess = Number(document.querySelector('.guess').value);
         console.log(guess, typeof guess);
 
+        //when there is no input
         if(!guess){
          document.querySelector('.message').textContent = 'No Answer'
         }
         
+        //when player wins
         else if(guess === secretNumber){
+            document.querySelector('.number').textContent = secretNumber
             document.querySelector('.message').textContent = 'correct Answer';
+
+            document.querySelector('body').style.backgroundColor = '#60b347';
+            document.querySelector('.number').style.width = '30rem'
         }
 
         else if (guess > secretNumber){
@@ -53,5 +59,22 @@ document.querySelector('.check').addEventListener(
        
         
     })
+
+    document.querySelector('.again').addEventListener(
+        'click', function(){
+          score =20;
+          secretNumber = Math.trunc(Math.random() *20)+1;
+          document.querySelector('.message').textContent = 'Start Guessing ..';
+          document.querySelector('.score').textContent = score;
+          document.querySelector('.number').textContent ='?';
+          document.querySelector('.guess').value=''
+          document.querySelector('body').style.backgroundColor = '#222';
+          document.querySelector('.number').style.width = '15rem'
+         
+          
+          
+
+        }
+    )
 
    
