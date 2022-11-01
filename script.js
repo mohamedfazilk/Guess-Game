64141a4df2718fb3922ad0120caecd072752d3f4
mohamedfazilk <2114921@cognizant.com>
2022-11-01 15:58:48 +0530

    add the score values and if else statements

--- script.js
+++ script.js
@@ -8,6 +8,7 @@
 // console.log(document.querySelector('.guess').value=24);
 
 const secretNumber = Math.trunc(Math.random() *20)+1;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber
 
 
@@ -21,6 +22,33 @@ document.querySelector('.check').addEventListener(
          document.querySelector('.message').textContent = 'No Answer'
         }
         
+        else if(guess === secretNumber){
+            document.querySelector('.message').textContent = 'correct Answer';
+        }
+
+        else if (guess > secretNumber){
+            if(score > 1){
+                document.querySelector('.message').textContent = 'Too High';
+                score--;
+                document.querySelector('.score').textContent = score;
+            }
+            else{
+                document.querySelector('.message').textContent = 'You lost the game';
+                document.querySelector('.score').textContent = 0;
+            }
+        }
+
+        else if (guess < secretNumber){
+            if(score > 1){
+                document.querySelector('.message').textContent = 'Too Low';
+                score--;
+                document.querySelector('.score').textContent = score;
+            }
+            else{
+                document.querySelector('.message').textContent = 'You lost the game';
+                document.querySelector('.score').textContent = 0;
+            }
+        }
         
        
         
